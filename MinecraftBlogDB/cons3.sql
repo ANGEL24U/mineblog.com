562e42e4b0c745914df583eee3771a2bed3c31bd
ANGEL24U <91678622+ANGEL24U@users.noreply.github.com>
2026-09-06 13:38:46 -0300

    Delete MinecraftBlogDB directory

--- MinecraftBlogDB/cons3.sql
+++
@@ -1,9 +0,0 @@
--- 1. Le ponemos la contraseña "diamante123" a Angel, copiando el hash de Steve
-UPDATE Usuarios
-SET contrasena_encriptada = (SELECT contrasena_encriptada FROM Usuarios WHERE nombre_usuario = 'Steve')
-WHERE nombre_usuario = 'Angel';
-
--- 2. Arreglamos a los usuarios nuevos para que no digan NULL y sean 'usuario'
-UPDATE Usuarios 
-SET rol = 'usuario' 
-WHERE rol IS NULL;
--- MinecraftBlogDB/cons4.sql
+++
@@ -1,7 +0,0 @@
--- Agregamos la columna 'estado', por defecto todos son 'activo'
-ALTER TABLE Usuarios ADD estado VARCHAR(20) DEFAULT 'activo';
-GO
-
--- Por si las dudas, nos aseguramos de que los usuarios viejos sean activos
-UPDATE Usuarios SET estado = 'activo' WHERE estado IS NULL;
-GO
